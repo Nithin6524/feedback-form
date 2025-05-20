ee13f5c7540b9f1fa279a7ceb7a39fda82b93680
M SAI NITHIN <nithin6524@gmail.com>
2025-05-21 02:19:24 +0530

    changed baseurl to backend dep url

--- Frontend/src/services/apiClient.js
+++ Frontend/src/services/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =  "http://localhost:7001";
+const BASE_URL =  "https://feedback-form-production-2722.up.railway.app/";
 
 export const api = axios.create({
     baseURL: BASE_URL,
