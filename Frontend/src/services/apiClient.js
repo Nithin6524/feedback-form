3ee65d072277134bb68c31f91af99eb69eb4c9a3
M SAI NITHIN <nithin6524@gmail.com>
2025-05-21 03:09:54 +0530

    updated env

--- Frontend/src/services/apiClient.js
+++ Frontend/src/services/apiClient.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
-const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+const BASE_URL = process.env.REACT_APP_BACKEND_URL || "KSJDFKSJD";
 
+
+console.log(BASE_URL);
 export const api = axios.create({
     baseURL: BASE_URL,
     headers: {
